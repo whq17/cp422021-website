4e3763a9aaa96af1fb087b9ef52faabcec1fe359
whq17 <whq210845@gmail.com>
2025-07-15 19:16:26 +0700

    lab3sussees

--- src/pages/tables.vue
+++ src/pages/tables.vue
@@ -2,16 +2,24 @@
 import InfoCard from "@/components/cards/InfoCard.vue";
 import TableInfoCard from "@/components/cards/TableInfoCard.vue";
 import { useTableStore } from "@/store/table";
+import { ref } from "vue";
 
 const tableStore = useTableStore();
-const reserveTable = (table) =>{
-  table.status = 'reserve'
-  table.checkin = new Date()
+
+// เก็บโต๊ะที่ถูกจองไว้ (หลายโต๊ะ)
+const reservedTables = ref([]);
+
+// ฟังก์ชันจองโต๊ะ เพิ่มโต๊ะลง reservedTables (ถ้ายังไม่จอง)
+const reserveTable = (table) => {
+  // ถ้ายังไม่อยู่ใน reservedTables ให้เพิ่ม
+  if (!reservedTables.value.includes(table)) {
+    table.status = "reserve";
+    table.checkin = new Date();
+    reservedTables.value.push(table);
+  }
 };
-
-
-
 </script>
+
 <template>
   <VCard>
     <VCardItem>
@@ -48,7 +56,7 @@ const reserveTable = (table) =>{
         </VCol>
         <VCol cols="3">
           <VCard class="align-center justify-center d-flex fill-height">
-            <VBtn                
+            <VBtn
               class="fill-height"
               variant="text"
               block
@@ -62,18 +70,42 @@ const reserveTable = (table) =>{
       </VRow>
     </VCardText>
   </VCard>
+
   <VCard class="mt-8">
     <VCardText>
       <VRow>
-      <VCol v-for="table in tableStore.tables" cols="3" class="d-flex align-center justify-center">
-        <v-btn @click="reserveTable(table)" size="x-large" block prepend-icon="mdi-table" height="200">
-        {{ table.name }} - {{ table.status }}</v-btn>
-      <TableInfoCard :table="table" />
-      </VCol>
-      
+        <VCol
+          v-for="table in tableStore.tables"
+          :key="table.id || table.name"
+          cols="3"
+          class="d-flex flex-column align-center justify-center"
+        >
+          <!-- ถ้าโต๊ะยังไม่จอง แสดงปุ่มจอง -->
+          <v-btn
+            v-if="!reservedTables.includes(table)"
+            @click="reserveTable(table)"
+            size="x-large"
+            block
+            prepend-icon="mdi-table"
+            height="200"
+          >
+            {{ table.name }} - {{ table.status }}
+          </v-btn>
 
+          <!-- ถ้าโต๊ะจองแล้ว แสดงการ์ด -->
+          <TableInfoCard v-else :table="table" />
+        </VCol>
       </VRow>
-      
-    </VCardText> 
-4  </VCard>
+
+      <!-- แสดงรายการโต๊ะที่ถูกจอง (ถ้าต้องการแสดงนอกตาราง) -->
+      <!--
+      <div class="mt-6">
+        <h3>โต๊ะที่ถูกจอง</h3>
+        <div v-for="table in reservedTables" :key="table.id || table.name">
+          <TableInfoCard :table="table" />
+        </div>
+      </div>
+      -->
+    </VCardText>
+  </VCard>
 </template>
